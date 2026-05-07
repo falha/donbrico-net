a8d24fbcab7b18a5798e64cde264cb09604e4d74
falha <dbricault@gmail.com>
2026-05-07 10:36:29 -0400

    fixing support

--- js/chatwoot.js
+++ js/chatwoot.js
@@ -1,34 +1,21 @@
-/**
- * Chatwoot website widget (donbrico.net).
- * Token: Chatwoot → Settings → Inboxes → Website channel → Installation.
- */
-(function () {
-  var CHATWOOT_WEBSITE_TOKEN = "Lh4G8U5jNtuBe8gEWnzW8i2j";
-  if (!CHATWOOT_WEBSITE_TOKEN) {
-    return;
-  }
-
-  window.chatwootSettings = {
-    position: "right",
-    type: "standard",
-    launcherTitle: "Chat with us",
-  };
-
-  var BASE_URL = "https://support.donbrico.net";
-  var d = document;
-  var t = "script";
-  var g = d.createElement(t);
-  var s = d.getElementsByTagName(t)[0];
-  g.src = BASE_URL + "/packs/js/sdk.js";
+(function(d,t) {
+  var BASE_URL="https://support.donbrico.net";
+  var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+  g.src=BASE_URL+"/packs/js/sdk.js";
   g.defer = true;
   g.async = true;
-  s.parentNode.insertBefore(g, s);
-  g.onload = function () {
-    if (window.chatwootSDK) {
-      window.chatwootSDK.run({
-        websiteToken: CHATWOOT_WEBSITE_TOKEN,
-        baseUrl: BASE_URL,
-      });
-    }
-  };
-})();
+  s.parentNode.insertBefore(g,s);
+  g.onload=function(){
+    window.chatwootSDK.run({
+      websiteToken: 'Lh4G8U5jNtuBe8gEWnzW8i2j',
+      baseUrl: BASE_URL
+    })
+  }
+})(document,"script");
+
+window.chatwootSettings = {
+  hideMessageBubble: false,
+  position: 'right',
+  locale: 'en',
+  type: 'standard'
+};
